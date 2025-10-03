ee842e4979539c89fd8cd53f59485bc1912e13f4
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-10-03 13:40:51 -0700

    Save changes

--- components/basecomponent.tsx
+++ components/basecomponent.tsx
@@ -8,6 +8,8 @@ export const BaseComponent = ({num, children}) => {
 
     console.log(`incomingContext inside Component Number ${num}: `, incomingContext);
     console.log("\n");
+
+    
     return (
       <div className="p-4 border rounded-sm">
         <p className="text-2xl"> I am Component Number: {num}</p>
